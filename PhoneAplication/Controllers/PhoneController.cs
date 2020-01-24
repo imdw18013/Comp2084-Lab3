8db12be418f4cee2c10280478ea30e9ef831ee42
Ajay Kumar <200434382@student.georgianc.on.ca>
2020-01-23 21:48:09 -0500

    first commit

---
+++ PhoneAplication/Controllers/PhoneController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneAplication.Models;
+
+
+namespace PhoneAplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var PhoneList = new List<phone>
+            {
+                new phone {PhoneName= "iphone pro", Manufaturer = "Apple", MSRP = "xyz", DateReleased = new DateTime(2020,02,06), ScreenSize = 4} ,
+                new phone {PhoneName= "Xperia 10 plus", Manufaturer = "Sony", MSRP = "xyz", DateReleased = new DateTime(2020,02,06), ScreenSize =6},
+                new phone {PhoneName= "Mogli420", Manufaturer = "Mogli", MSRP = "xyz", DateReleased = new DateTime(2020,10,16), ScreenSize =4},
+                new phone {PhoneName= "S10", Manufaturer = "Samsung", MSRP = "xyz", DateReleased = new DateTime(2020,04,26), ScreenSize = 7 },
+
+            };
+            return View(PhoneList);
+        }
+    }
+}
---
+++ PhoneAplication/Models/phone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneAplication.Models
+{
+    public class phone
+    {
+        public phone()
+        {
+            PhoneName = "";
+            Manufaturer = "";
+            DateReleased = new DateTime();
+        }
+        public int PhoneId { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufaturer { get; set; }
+        public string MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+
+}
